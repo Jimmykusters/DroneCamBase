5532b165fa137170bf2f21b09155a24b007ffd76
Jimmy Kusters <Jimmy.kusters@hotmail.com>
2020-12-16 17:06:33 +0100

    Update picamV4.py

--- picamV4.py
+++ picamV4.py
@@ -30,17 +30,17 @@ def main():
         frame = cam.capture()
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+        #blurred = cv2.GaussianBlur(gray, (11, 11), 0)
 
         # threshold the image to reveal light regions in the
         # blurred image
-        thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1]
 
         # perform a series of erosions and dilations to remove
         # any small blobs of noise from the thresholded image
         thresh = cv2.erode(thresh, None, iterations=2)
         thresh = cv2.dilate(thresh, None, iterations=4)
-        
+        cv2.imshow("Thresh hold image", grays)
         cv2.imshow("Thresh hold image", thresh)
 
         #To be able to stop the programm
