88a00e41a15d3e1f3f05663d045f094c1eedcbef
Jimmy Kusters <Jimmy.kusters@hotmail.com>
2020-12-16 20:00:46 +0100

    Update picamV4.py

--- picamV4.py
+++ picamV4.py
@@ -64,7 +64,7 @@ def main():
 
             (x, y, w, h) = cv2.boundingRect(c)
             ((cX, cY), radius) = cv2.minEnclosingCircle(c)
-            if radius > 10:
+            if radius < 10:
                 cv2.circle(frame, (int(cX), int(cY)), int(radius), (0, 0, 255), 3)
                 cv2.putText(frame, "#{}".format(i + 1), (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
         # show the output image
