a73b6ed6ed9e54f4735eea1f3ab3611e6141d09e
Jimmy Kusters <Jimmy.kusters@hotmail.com>
2020-12-16 19:29:46 +0100

    Update picamV4.py

--- picamV4.py
+++ picamV4.py
@@ -18,11 +18,6 @@ class USBcamControl():
         self.frame = np.array(self.raw)
         return self.frame
 
-
-
-
-
-
 def main():
     cam           = USBcamControl()
 
@@ -36,13 +31,33 @@ def main():
         # blurred image
         thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1]
 
+        # perform a connected component analysis on the thresholded
+        # image, then initialize a mask to store only the "large"
+        # components
+        labels = measure.label(thresh, neighbors=8, background=0)
+        mask = np.zeros(thresh.shape, dtype="uint8")
+        # loop over the unique components
+        for label in np.unique(labels):
+            # if this is the background label, ignore it
+            if label == 0:
+                continue
+            # otherwise, construct the label mask and count the
+            # number of pixels 
+            labelMask = np.zeros(thresh.shape, dtype="uint8")
+            labelMask[labels == label] = 255
+            numPixels = cv2.countNonZero(labelMask)
+            # if the number of pixels in the component is sufficiently
+            # large, then add it to our mask of "large blobs"
+            if numPixels > 10:
+                mask = cv2.add(mask, labelMask)
+
         # perform a series of erosions and dilations to remove
         # any small blobs of noise from the thresholded image
         # thresh = cv2.erode(thresh, None, iterations=2)
         # thresh = cv2.dilate(thresh, None, iterations=4)
-        cv2.imshow("frame hold image", frame)
+        cv2.imshow("frame hold image", thresh)
 
-        cv2.imshow("Thresh hold image", thresh)
+        cv2.imshow("Thresh hold image", mask)
 
         #To be able to stop the programm
         key = cv2.waitKey(1) & 0xFF
