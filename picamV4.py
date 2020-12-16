d0ea3b2041d9c4775f1590f6eb0f4b15de7ca18b
Jimmy Kusters <Jimmy.kusters@hotmail.com>
2020-12-16 17:08:06 +0100

    Update picamV4.py

--- picamV4.py
+++ picamV4.py
@@ -40,7 +40,8 @@ def main():
         # any small blobs of noise from the thresholded image
         thresh = cv2.erode(thresh, None, iterations=2)
         thresh = cv2.dilate(thresh, None, iterations=4)
-        cv2.imshow("Thresh hold image", gray)
+        cv2.imshow("gray hold image", gray)
+        
         cv2.imshow("Thresh hold image", thresh)
 
         #To be able to stop the programm
