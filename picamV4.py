95e7cb55106a361783d4a3a1266e0908bc3127e9
Jimmy Kusters <Jimmy.kusters@hotmail.com>
2020-12-18 14:58:30 +0100

    Update picamV4.py

--- picamV4.py
+++ picamV4.py
@@ -109,7 +109,7 @@ def findContours(frame, mask):
         (x, y, w, h) = cv2.boundingRect(c)
         ((cX, cY), radius) = cv2.minEnclosingCircle(c)
         if radius < 10:
-            cv2.circle(frame, (int(cX), int(cY)), int(radius), (0, 0, 255), 2)
+            #cv2.circle(frame, (int(cX), int(cY)), int(radius), (0, 0, 255), 2)
             #cv2.putText(frame, "#{}".format(i+1), (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 1)
             #Add point to list
             points.append(point(cX, cY, i+1))
